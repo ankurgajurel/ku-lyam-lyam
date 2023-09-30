fbdab657f8fddac603fa52009ffae5b413a8191c
CaffeineDuck <hello@samrid.me>
2023-10-01 01:24:19 +0545

    fix: cors not working

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { flowController } from "./flow";
 import { claimsRouter } from "./claims";
 import { onboardController } from "./onboard";
@@ -8,6 +9,7 @@ const app = express();
 const port = parseInt(process.env.PORT || "8080");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/flow", flowController);
 app.use("/claims", claimsRouter);
