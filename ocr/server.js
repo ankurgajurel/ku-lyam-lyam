575ea576d653bcf49b72d88f069c725cae969ae5
entalecia <64960028+entalecia@users.noreply.github.com>
2023-10-01 08:50:07 +0545

    i want to die (this is not a joke)

--- ocr/server.js
+++ ocr/server.js
@@ -10,7 +10,8 @@ dotenv.config();
 
 const app = express();
 const port = 6970
-
+const SERVER_URL = process.env.SERVER_URL || "https://server-p7.samrid.me";
+const API_KEY = process.env.API_KEY || "InternalKey@1234";
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 
@@ -20,28 +21,24 @@ app.post('/ocr', upload.single('citizenship'), async(req, res) => {
   const userId = req.body.userId;
   const iv = req.body.iv;
   const decryptionKey = req.body.decryptionKey;
-  // let publicKey = req.body.publicKey; //length 4096
-  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
-    modulusLength: 4096,
-  });
+  let publicKey = req.body.publicKey; //length 4096
   
   const receiveData = await OCR(citizenship.buffer);
   
-  const encryptedData = encryptWithPublicKey(publicKey, JSON.stringify(receiveData)).toString("hex");
-  
-  const apiUrl = process.env.SERVER_URL + "/onboard/verify";
+  // const encryptedData = encryptWithPublicKey(publicKey, JSON.stringify(receiveData)).toString("hex");
+  const apiUrl = SERVER_URL + "/onboard/verify";
   const headers = {
-    'x-api-key': process.env.API_KEY,
+    'x-api-key': API_KEY,
     'Content-Type': 'application/json',
   };
   const sendData = {
     userId: userId,
     decryptionKey: decryptionKey,
     iv: iv,
-    encryptedData: encryptedData,
+    encryptedData: JSON.stringify(receiveData),
   };
-  // console.log('API Request:', sendData)    
-  const response = await axios.patch(apiUrl, sendData, { headers });
+  console.log('API Request:', sendData)    
+  // const response = await axios.patch(apiUrl, sendData, { headers });
 
   if (!isAuthorized()) {
     res.status(401).send('Unauthorized');
@@ -49,31 +46,38 @@ app.post('/ocr', upload.single('citizenship'), async(req, res) => {
   }
   res.status(200).send("Authorized");
 
-  let GetResponse = await fetch("https://server-p7.samrid.me/claims/types");
-  let data = await GetResponse.json();
 
-  let claims = {
-    "Above 21": false,
-    "Nepali Citizen": true,
-    "Verified": true
+  try{
+    console.log("tessssttt");
+    let GetResponse = await fetch("https://server-p7.samrid.me/claims/types");
+    let data = await GetResponse.json();
+
+    let claims = {
+      "Above 21": false,
+      "Nepali Citizen": true,
+      "Verified": true
+    }
+    
+    if(getAge(receiveData.dateOfBirth) < 21){
+      claims["Above 21"] = false;
+    }
+    else{
+      claims["Above 21"] = true;
+    }
+    
+    const requests = data.claimTypes.map(item => {
+      let temp = {
+        userId: userId,
+        claimTypeId: item.id,
+        value: claims[item.name]
+      };
+      const claimsUrl = SERVER_URL + "/claims/add";
+      axios.post(claimsUrl, JSON.stringify(temp), { headers }).then(console.log).catch(console.log);
+    });
   }
-  
-  if(getAge(receiveData.dateOfBirth) < 21){
-    claims["Above 21"] = false;
-  }
-  else{
-    claims["Above 21"] = true;
-  }
-  
-  const requests = data.claimTypes.map(item => {
-    let temp = {
-      userId: userId,
-      claimTypeId: item.id,
-      value: claims[item.name]
-    };
-    const claimsUrl = process.env.SERVER_URL + "/claims/add";
-    axios.post(claimsUrl, JSON.stringify(temp), { headers }).then(console.log).catch(console.log);
-  });
+  catch (err){
+    console.log(err);
+  };
   
 });
 
